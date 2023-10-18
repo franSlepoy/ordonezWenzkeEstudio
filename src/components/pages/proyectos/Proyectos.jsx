e6fa3943c60edd6fdd4ab8444c032296ab1c42c8
franSlepoy <slepoyfrancisco@gmail.com>
2023-10-17 22:28:55 -0300

    terminando grilla de proyectos

--- src/components/pages/proyectos/Proyectos.jsx
+++ src/components/pages/proyectos/Proyectos.jsx
@@ -1,31 +1,63 @@
-import { Box, ImageList, ImageListItem } from '@mui/material'
-import React from 'react'
-import { ProyectosData } from '../../../servidor/ProyectosData'
+import { useState } from 'react';
+import { Box, ImageList, ImageListItem, Typography } from '@mui/material';
+import { ProyectosData } from '../../../servidor/ProyectosData';
 
 const Proyectos = () => {
+  const [hoveredItem, setHoveredItem] = useState(null);
+
   return (
     <>
-    <Box  width={"80%"} m={"auto"} >
-    <ImageList variant="classic" cols={3} gap={0}>
-  {ProyectosData.map((item) => (
-    <ImageListItem key={item.id}>
-      <Box sx={{maxWidth:"340px", p:4, borderLeft:"solid black 1px", borderRight:"solid black 1px"}}>
-      <img
-        width={"100%"}
-        srcSet={`${item.imagen}?w=248&fit=crop&auto=format&dpr=2 2x`}
-        src={`${item.imagen}?w=248&fit=crop&auto=format`}
-        alt={item.titulo}
-        loading="lazy"
-      />
+      <Box width={"95%"} m={"auto"} sx={{ borderLeft: "solid #666666 0.1px", borderRight: "solid #666666 0.1px" }}>
+        <ImageList variant="classic" cols={3} gap={0}>
+          {ProyectosData.map((item) => (
+            <ImageListItem key={item.id} onMouseEnter={() => setHoveredItem(item)} onMouseLeave={() => setHoveredItem(null)}>
+              <a href={`/descripcion/${item.id}`} style={{ textDecoration: 'none', position: 'relative' }}>
+                <Box
+                  sx={{
+                    p: 4,
+                    borderLeft: "solid #666666 0.1px",
+                    borderRight: "solid #666666 0.1px",
+                    textAlign: "center",
+                    position: 'relative',
+                    height:"100%"
+                  }}
+                >
+                  {hoveredItem === item ? (
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        margin:"auto",
+                        width: '75%',
+                        height:"380px",
+                        backgroundColor: '#e2e2c7',
+                        transition: 'background-color 0.5s',
+                        
+                        }}
+                    >
+                      <Typography sx={{fontFamily:"Work Sans ", textAlign:"center", fontSize:"16px", color:"black"  }} > 
+                      {item.titulo}
+                      </Typography>
+                      
+                    </Box>
+                  ) : (
+                    <img
+                      width={"75%"}
+                      srcSet={`${item.imagen}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                      src={`${item.imagen}?w=248&fit=crop&auto=format`}
+                      alt={item.titulo}
+                      loading="lazy"
+                    />
+                  )}
+                </Box>
+              </a>
+            </ImageListItem>
+          ))}
+        </ImageList>
       </Box>
-    
-    </ImageListItem>
-  ))}
-</ImageList>
-    </Box>
     </>
-   
-  )
-}
+  );
+};
 
-export default Proyectos
+export default Proyectos;
