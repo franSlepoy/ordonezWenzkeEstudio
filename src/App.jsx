b544a6c2f9244e6b92862c0550e149a8e9724c75
axelriveroc <riverocoronelaxel@gmail.com>
2023-10-22 19:43:05 -0300

    la rama master es la ppal ahora nuevamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./components/pages/home/Home"
-import Descripcion from "./components/pages/descripcion/Descripcion"
-import NavBar from "./components/commond/navBar/NavBar"
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/home/Home";
+import Descripcion from "./components/pages/descripcion/Descripcion";
+import NavBar from "./components/commond/navBar/NavBar";
 
 function App() {
-  
-
   return (
     <BrowserRouter>
-    
-     <Routes>
-      
-      <Route path='/' element={<Home/>}/>
-      <Route path='/description' element={<Descripcion/>}/>
-      
-
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path='/description' element={<Descripcion/>}/> */}
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
