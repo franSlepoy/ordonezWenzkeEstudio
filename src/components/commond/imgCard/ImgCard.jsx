a8ca5fed7974dd9b61f51062e03c4dc8d95dcbc8
franSlepoy <slepoyfrancisco@gmail.com>
2023-10-24 09:56:23 -0300

    imagen apaisada

--- src/components/commond/imgCard/ImgCard.jsx
+++ src/components/commond/imgCard/ImgCard.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ImgCard = (props) => {
   return (
     <Box  height={"580px"} >
-      <img style={{width:"100%", height:"580px", objectFit:"contain"}} src={props.imagen} alt="" />
+      <img style={{width:"100%", height:"580px"}} src={props.imagen} alt="" />
     </Box>
   );
 };
