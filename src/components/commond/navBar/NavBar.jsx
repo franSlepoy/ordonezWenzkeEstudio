09d69e64014c186a163dd5e63b30f76a7f26ea7a
axelriveroc <riverocoronelaxel@gmail.com>
2023-10-25 16:11:06 -0300

    arreglando home

--- src/components/commond/navBar/NavBar.jsx
+++ src/components/commond/navBar/NavBar.jsx
@@ -1,156 +1,186 @@
 import {
-  AppBar,
-  Box,
-  Button,
-  Drawer,
-  Grid,
-  Hidden,
-  Toolbar,
-  Typography,
+	AppBar,
+	Box,
+	Button,
+	Drawer,
+	Grid,
+	Hidden,
+	Toolbar,
+	Typography,
 } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import MenuIcon from "@mui/icons-material/Menu";
 import NavListDrawer from "./NavListDrawe";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import logo_ow_negro from "../../../../public/imagenes/OWnegro.png";
+import StylesNavBar from "./NavBar.module.css";
 
 const NavBar = () => {
-  const { t, i18n } = useTranslation();
+	const { t, i18n } = useTranslation();
 
-  // Función para cambiar el idioma a español
-  const cambiarAEspanol = () => {
-    i18n.changeLanguage("es");
-  };
+	// Función para cambiar el idioma a español
+	const cambiarAEspanol = () => {
+		i18n.changeLanguage("es");
+	};
 
-  // Función para cambiar el idioma a inglés
-  const cambiarAIngles = () => {
-    i18n.changeLanguage("en");
-  };
+	// Función para cambiar el idioma a inglés
+	const cambiarAIngles = () => {
+		i18n.changeLanguage("en");
+	};
 
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const cerrarBar = ()=> {
-    return setOpen(false)
-  }
+	const cerrarBar = () => {
+		return setOpen(false);
+	};
 
-  return (
-    <>
-      <Hidden mdDown>
-        <AppBar position="fixed" sx={{ boxShadow: 0, bgcolor: "white" }}>
-          <Toolbar sx={{ justifyContent: "space-between" }}>
-            <Box /* width={"300px"} */ display={"flex"}>
-              <a
-                href="#projects"
-                style={{
-                  fontSize: "16px",
-                  color: "#000",
-                  underline: "none",
-                  textDecoration: "none",
-                }}
-              >
-                {t("tituloNavBarIzquierda")}
-              </a>
-              <Box
-                sx={{
-                  ml: 2,
-                  width: "20px",
-                  height: "20px",
-                  borderRadius: "100%",
-                  bgcolor: "#e2e2c7",
-                }}
-              ></Box>
-            </Box>
+	const [isScrolled, setIsScrolled] = useState(false);
 
-            <Box >
-              <img width={"300px"} src="/imagenes/ownegro.png" alt="" />
-            </Box>
+	useEffect(() => {
+		const handleScroll = () => {
+			const isScrolled = window.scrollY > window.innerHeight * 0.3;
+			setIsScrolled(isScrolled);
+		};
 
-            <Box /* width={"300px"} */>
-              <a
-                href="#studio"
-                style={{
-                  textAlign: "end",
-                  fontSize: "16px",
-                  color: "#000",
-                  underline: "none",
-                  textDecoration: "none",
-                }}
-              >
-                {t("tituloNavBarDerecha")}
-              </a>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </Hidden>
+		document.addEventListener("scroll", handleScroll);
+		return () => {
+			document.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-      <Hidden mdDown>
-        <AppBar position="fixed" sx={{ width: "100%" }}>
-          <Grid
-            container
-            justifyContent="space-between"
-            sx={{ position: "fixed", top: "50%" }}
-          >
-            <Grid item>
-              <Button onClick={cambiarAEspanol} data-i18n="es">
-                <Box
-                  sx={{
-                    fontFamily: "sans-serif",
-                    fontSize: "12px",
-                    lineHeight: "12px",
-                    color: "#1d1d1d",
-                  }}
-                >
-                  <Typography>e</Typography>
-                  <Typography>s</Typography>
-                </Box>
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button onClick={cambiarAIngles} data-i18n="en">
-                <Box
-                  sx={{
-                    fontFamily: "sans-serif",
-                    fontSize: "12px",
-                    lineHeight: "12px",
-                    color: "#1d1d1d",
-                  }}
-                >
-                  <Typography>e</Typography>
-                  <Typography>n</Typography>
-                </Box>
-              </Button>
-            </Grid>
-          </Grid>
-        </AppBar>
-      </Hidden>
+	return (
+		<>
+			{/* VERSION DESKTOP NAVBAR */}
+			<Hidden mdDown>
+				<div style={{ overflow: "hidden" }}>
+					<AppBar
+						position="fixed"
+						sx={{
+							boxShadow: 0,
+							bgcolor: "white",
+							top: isScrolled ? "0" : "-64px",
+							transition: "top 0.3s",
+						}}
+					>
+						<Toolbar sx={{ justifyContent: "space-between" }}>
+							<Box /* width={"300px"} */ display={"flex"}>
+								<a
+									href="#projects"
+									style={{
+										fontSize: "16px",
+										color: "#000",
+										underline: "none",
+										fontFamily: "Pragmatica",
+										textDecoration: "none",
+									}}
+								>
+									{t("tituloNavBarIzquierda")}
+								</a>
+								<Box
+									sx={{
+										ml: 2,
+										width: "20px",
+										height: "20px",
+										borderRadius: "100%",
+										bgcolor: "#e2e2c7",
+									}}
+								></Box>
+							</Box>
 
-      <Hidden mdUp>
-        <AppBar position="fixed" sx={{ boxShadow: 0, bgcolor: "white" }}>
-          <Toolbar sx={{ justifyContent: "space-between" }}>
-            <Box>
-              <img width={"50%"} src="/imagenes/ownegro.png" alt="" />
-            </Box>
-            <Box>
-              <Button
-                onClick={() => setOpen(true)}
-                sx={{
-                  color: "#1d1d1d",
-                  "&:hover": {
-                    backgroundColor: "transparent",
-                  },
-                }}
-              >
-                <MenuIcon fontSize="large" />
-              </Button>
-            </Box>
-          </Toolbar>
-        </AppBar>
+							<Box>
+								<img width={"300px"} src={logo_ow_negro} alt="" />
+							</Box>
 
-        <Drawer open={open} anchor="top" onClose={() => setOpen(false)}>
-          <NavListDrawer onclose={cerrarBar} />
-        </Drawer>
-      </Hidden>
-    </>
-  );
+							<Box /* width={"300px"} */>
+								<a
+									href="#studio"
+									style={{
+										textAlign: "end",
+										fontSize: "16px",
+										color: "#000",
+										underline: "none",
+										fontFamily: "Pragmatica",
+										textDecoration: "none",
+									}}
+								>
+									{t("tituloNavBarDerecha")}
+								</a>
+							</Box>
+						</Toolbar>
+					</AppBar>
+				</div>
+			</Hidden>
+			{/* MANEJADOR DE LENGUAJES
+			 */}{" "}
+			<Hidden mdDown>
+				<AppBar position="fixed" sx={{ width: "100%" }}>
+					<Grid
+						container
+						justifyContent="space-between"
+						sx={{ position: "fixed", top: "50%" }}
+					>
+						<Grid item>
+							<Button onClick={cambiarAEspanol} data-i18n="es">
+								<Box
+									sx={{
+										fontFamily: "sans-serif",
+										fontSize: "12px",
+										lineHeight: "12px",
+										color: "#1d1d1d",
+									}}
+								>
+									<Typography>e</Typography>
+									<Typography>s</Typography>
+								</Box>
+							</Button>
+						</Grid>
+						<Grid item>
+							<Button onClick={cambiarAIngles} data-i18n="en">
+								<Box
+									sx={{
+										fontFamily: "sans-serif",
+										fontSize: "12px",
+										lineHeight: "12px",
+										color: "#1d1d1d",
+									}}
+								>
+									<Typography>e</Typography>
+									<Typography>n</Typography>
+								</Box>
+							</Button>
+						</Grid>
+					</Grid>
+				</AppBar>
+			</Hidden>
+			{/* VERSION MOBILE NAVBAR */}
+			<Hidden mdUp>
+				<AppBar position="fixed" sx={{ boxShadow: 0, bgcolor: "white" }}>
+					<Toolbar sx={{ justifyContent: "space-between" }}>
+						<Box>
+							<img width={"50%"} src="/imagenes/ownegro.png" alt="" />
+						</Box>
+						<Box>
+							<Button
+								onClick={() => setOpen(true)}
+								sx={{
+									color: "#1d1d1d",
+									"&:hover": {
+										backgroundColor: "transparent",
+									},
+								}}
+							>
+								<MenuIcon fontSize="large" />
+							</Button>
+						</Box>
+					</Toolbar>
+				</AppBar>
+
+				<Drawer open={open} anchor="top" onClose={() => setOpen(false)}>
+					<NavListDrawer onclose={cerrarBar} />
+				</Drawer>
+			</Hidden>
+		</>
+	);
 };
 
 export default NavBar;
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -5,51 +5,54 @@ import NavBar from "../../commond/navBar/NavBar";
 import StudioContainer from "../studio/StudioContainer";
 
 const Home = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [isTitleVisible, setIsTitleVisible] = useState(true);
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentPosition = window.scrollY;
-      setIsTitleVisible(currentPosition <= scrollPosition); // Desplazándose hacia arriba
-      setScrollPosition(currentPosition);
-    };
+	const [scrollPosition, setScrollPosition] = useState(0);
+	const [isTitleVisible, setIsTitleVisible] = useState(true);
+	useEffect(() => {
+		const handleScroll = () => {
+			const currentPosition = window.scrollY;
+			setIsTitleVisible(currentPosition <= scrollPosition); // Desplazándose hacia arriba
+			setScrollPosition(currentPosition);
+		};
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [scrollPosition]);
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [scrollPosition]);
 
-  return (
-    <>
-      <Box sx={{ zIndex: 3 }}>
-        <img
-          height="766px"
-          width="100%"
-          src="public/imagenes/1673364522_1.jpg"
-          alt="una casa en la playa"
-        />
-      </Box>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          zIndex: 4,
-          opacity: isTitleVisible ? 1 : 0,
-          transition: "opacity 0.3s",
-        }}
-      >
-        <img width={"100%"} src="public/imagenes/owblanco.png" alt="" />
-      </Box>
+	return (
+		<>
+			{/* IMAGEN DE ARRIBA CON EL LOGO */}
+			<Box sx={{ zIndex: 333 }}>
+				<img
+					/* height="766px" */
+					width="100%"
+					src="public/imagenes/1673364522_1.jpg"
+					alt="una casa en la playa"
+					style={{ height: "100vh" }}
+				/>
+			</Box>
+			<Box
+				sx={{
+					position: "absolute",
+					top: "50%",
+					left: "50%",
+					transform: "translate(-50%, -50%)",
+					zIndex: 4,
+					opacity: isTitleVisible ? 1 : 0,
+					transition: "opacity 0.3s",
+				}}
+			>
+				<img width={"100%"} src="public/imagenes/owblanco.png" alt="" />
+			</Box>
 
-      <NavBar />
-      <Proyectos />
-      <StudioContainer />
-      
-    </>
-  );
+			{/*       empieza el navbar y el resto
+			 */}
+			<NavBar />
+			<Proyectos />
+			<StudioContainer />
+		</>
+	);
 };
 
 export default Home;
